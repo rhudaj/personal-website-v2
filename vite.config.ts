e7b466c77e3e656b3c3847c83a91f55e80e507e0
Romanhudaj11 <romanhudaj1@gmail.com>
2025-01-24 00:24:53 -0500

    added project

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// vite.config.js
-export default defineConfig({
-  plugins: [react()],
-  base: "/", //note!!!! this was a solution to a blank page when releasing to a custom domain
-})
